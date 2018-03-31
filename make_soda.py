552ed6cdf2163cea744ddaf49f343df5347359a2
Meggan M Green <github@meggangreen.com>
2018-03-31 04:38:15 +0000

    restore changes from mmg

--- make_soda.py
+++ make_soda.py
@@ -18,17 +18,32 @@ def make_soda(num, sizes):
                 temp.append(item)
                 rem = num % item
                 num = rem
-    if temp not in combos:
-        combos.append(temp)
-    else:
-        return combos
+    return temp
+    # if temp not in combos:
+    #     combos.append(temp)
     # refactor to go again
     # keep those in combo list, check if in combo list
 
     return combos
-print make_soda(4, [1, 2, 3])
-print make_soda(4, [2, 4])
+# print make_soda(4, [1, 2, 3])
+# print make_soda(4, [2, 4])
 # [[1,1,1,1], [1,1,2], [1,3], [2, 2]]
+
+
+def get_all_combos(num, sizes):
+
+    all_combos = []
+    for i in range(len(sizes)):
+        new_combo = make_soda(num, sizes[:i+1])
+        print num, sizes[:i+1], new_combo
+        all_combos.append(new_combo)
+
+    return all_combos
+
+
+
+
+
 
 def all_soda(num, sizes):
     """take in number and soda sizes, output combos"""
@@ -41,9 +56,10 @@ def all_soda(num, sizes):
         item = sizes[i]
         if num % item == 0:
             pack = num / item
-            while pack > 0:
-                temp.append(item)
-                pack -= 1
+            temp.extend([item]*pack)
+            # while pack > 0:
+            #     temp.append(item)
+            #     pack -= 1
         else:
             if item <= num:
                 temp.append(item)
@@ -52,4 +68,6 @@ def all_soda(num, sizes):
                 # recursive call all_soda here, passing num and
                 # the sliced list up to this 'i'
         combos.append(temp)
+
+    return combos
 
